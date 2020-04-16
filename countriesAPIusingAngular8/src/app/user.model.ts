877fb2de354ccc60b159f8ec49773e90594e8ed3
barathviknesh <barathviknesh.9566@gmail.com>
2020-04-16 16:27:01 +0530

    API-A8

---
+++ countriesAPIusingAngular8/src/app/user.model.ts
@@ -0,0 +1,26 @@
+export class User {
+    // id: number;
+    name : string;
+    capital : string;
+    region : string;
+    population : number;
+    flag : string;
+    // username : string;
+    // email : string;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
